aa246bdee156b9a972a9a3449b59ec0d0836380c
hurkatolto <hurkatolto1@gmail.com>
2014-08-14 16:39:00 +0200

    Add some missing functions to ec_board.hrl.

--- apps/ec_chess/src/ec_board.erl
+++ apps/ec_chess/src/ec_board.erl
@@ -1,37 +1,28 @@
 -module(ec_board).
 
+-compile([native, inline, {inline_size, 300}]).
+
 -export([
-    op_cond/2,
+    op_cond/3,
     init_board/0,
-    start_board/0,
-    perftest/3
+    start_board/0
 ]).
 
-
--compile(inline).
--compile({inline_size, 200}).
-
 -include("ec.hrl").
+-include("ec_perf.hrl").
+
 %%------------------------------------------------------------------------------
 %% API
 %%------------------------------------------------------------------------------
 
-
--spec perftest(C :: integer(), State :: #board_state{}, Op :: operator()) ->
-    Result :: integer().
-perftest(0, _State, Op) ->
-    ok;
-perftest(C, State, Op) ->
-    ec_board:op_cond(State, Op),
-    perftest(C - 1, State, Op).
-
--spec op_cond(State, Op) -> boolean() when
+-spec op_cond(State, Op, CheckAfter) -> boolean() when
     State :: #board_state{},
-    Op :: operator().
-op_cond(#board_state{} = State, Op) ->
+    Op :: operator(),
+    CheckAfter :: boolean().
+op_cond(#board_state{} = State, Op, CheckAfter) ->
     case check_not_equal(Op) andalso check_field_limits(Op) of
         true ->
-            op_cond2(State, Op);
+            op_cond2(State, Op, CheckAfter);
         false ->
             false
     end.
@@ -61,10 +52,12 @@ init_board() ->
 %% Internal functions
 %%------------------------------------------------------------------------------
 
--spec op_cond2(State, Op) -> boolean() when
+-spec op_cond2(State, Op, CheckAfter) -> boolean() when
     State :: #board_state{},
-    Op :: operator().
-op_cond2(#board_state{board = Board, to_move = ToMove} = State, Op) ->
+    Op :: operator(),
+    CheckAfter :: boolean().
+op_cond2(#board_state{board = Board, to_move = ToMove} = State, Op,
+         CheckAfter) ->
     case {get_piece(Board, element(1, Op)), get_piece(Board, element(2, Op))} of
         {?OO, _} ->
             false;
@@ -77,23 +70,24 @@ op_cond2(#board_state{board = Board, to_move = ToMove} = State, Op) ->
                 {Col, _} when Col =/= ToMove ->
                     false;
                 {Col, _} ->
-                    op_cond(FromPiece, State, Op) andalso
-                        not check_chess_after_op(State, Op, Col)
+                    op_cond3(FromPiece, State, Op) andalso
+                        (not CheckAfter or
+                             not check_chess_after_op(State, Op, Col))
             end
     end.
 
--spec op_cond(FromPiece, State, Op) -> Result when
+-spec op_cond3(FromPiece, State, Op) -> Result when
       State :: #board_state{},
       Op :: operator(),
       FromPiece :: integer(),
       Result :: boolean().
 %% White pawn
-op_cond(?WP, State, Op) ->
+op_cond3(?WP, State, Op) ->
     Col = color(?WP),
     is_pawn_simple_move(State, Op, Col) orelse is_pawn_take(State, Op, Col)
         orelse is_en_passant(State, Op, Col);
 %% White rook
-op_cond(?WR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?WR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case Fx =:= Tx orelse Fy =:= Ty of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -101,11 +95,11 @@ op_cond(?WR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% White knight
-op_cond(?WN, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?WN, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     (diff(Tx, Fx) =:= 1 andalso diff(Ty, Fy) =:= 2) orelse
        (diff(Tx, Fx) =:= 2 andalso diff(Ty, Fy) =:= 1);
 %% White bishop
-op_cond(?WB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?WB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case diff(Fx, Tx) =:= diff(Fy, Ty) of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -113,7 +107,7 @@ op_cond(?WB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% White queen
-op_cond(?WQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?WQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case diff(Fx, Tx) =:= diff(Fy, Ty) orelse Fx =:= Tx orelse Fy =:= Ty of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -121,17 +115,17 @@ op_cond(?WQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% White king
-op_cond(?WK, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?WK, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     Dx = diff(Fx, Tx),
     Dy = diff(Fy, Ty),
     (Dx =:= 1 orelse Dx =:= 0) andalso (Dy =:= 1 orelse Dy =:= 0);
 %% Black pawn
-op_cond(?BP, State, Op) ->
+op_cond3(?BP, State, Op) ->
     Col = color(?BP),
     is_pawn_simple_move(State, Op, Col) orelse is_pawn_take(State, Op, Col)
         orelse is_en_passant(State, Op, Col);
 %% Black rook
-op_cond(?BR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?BR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case Fx =:= Tx orelse Fy =:= Ty of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -139,11 +133,11 @@ op_cond(?BR, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% Black knight
-op_cond(?BN, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?BN, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     (diff(Tx, Fx) =:= 1 andalso diff(Ty, Fy) =:= 2) orelse
        (diff(Tx, Fx) =:= 2 andalso diff(Ty, Fy) =:= 1);
 %% Black bishop
-op_cond(?BB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?BB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case diff(Fx, Tx) =:= diff(Fy, Ty) of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -151,7 +145,7 @@ op_cond(?BB, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% Black queen
-op_cond(?BQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?BQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     case diff(Fx, Tx) =:= diff(Fy, Ty) orelse Fx =:= Tx orelse Fy =:= Ty of
         true ->
             check_empty_fields_between(State#board_state.board, Fx, Fy, Tx, Ty);
@@ -159,14 +153,15 @@ op_cond(?BQ, State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
             false
     end;
 %% Black king
-op_cond(?BK, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
+op_cond3(?BK, _State, {{Fx, Fy}, {Tx, Ty}} = _Op) ->
     Dx = diff(Fx, Tx),
     Dy = diff(Fy, Ty),
     (Dx =:= 1 orelse Dx =:= 0) andalso (Dy =:= 1 orelse Dy =:= 0).
 
 -spec get_piece(Board :: board(), Pos :: position() | integer()) -> integer().
 get_piece(Board, {X, Y}) ->
-    element(?POS(X, Y), Board);
+    Pos = ?POS(X, Y),
+    element(Pos, Board);
 get_piece(Board, Pos) ->
     element(Pos, Board).
 
@@ -239,14 +234,140 @@ between(_V, _Min, _Max) -> false.
     Op :: operator(),
     Color :: color(),
     Result :: boolean().
-check_chess_after_op(State, _Op, Col) ->
+check_chess_after_op(State, Op, Col) ->
+    NewState = apply_op(State, Op),
     King = case Col of
                ?WHITE -> ?WK;
                ?BLACK -> ?BK
            end,
-    KingPos = find_first_piece(State#board_state.board, King),
-    false.
+    KingPos = find_first_piece(NewState#board_state.board, King),
+    is_in_check(1, NewState, KingPos, ?NEG_COL(Col)).
 
+is_in_check(65, _State, _KingPos, _OppCol) ->
+    false;
+is_in_check(C, State, KingPos, OppCol) ->
+    FromPos = ?UN_POS(C),
+    Col = color(get_piece(State#board_state.board, FromPos)),
+    case Col =:= OppCol of
+        true ->
+            Op = {FromPos, KingPos},
+            case op_cond(State, Op, _CheckAfter = false) of
+                true -> true;
+                false -> is_in_check(C + 1, State, KingPos, OppCol)
+            end;
+        false ->
+            is_in_check(C + 1, State, KingPos, OppCol)
+    end.
+
+-spec apply_op(State, Op) -> NewState when
+      State :: #board_state{},
+      Op :: operator(),
+      NewState :: #board_state{}.
+apply_op(State, {{Fx, Fy}, {_Tx, _Ty}} = Op) ->
+    Piece = get_piece(State#board_state.board, {Fx, Fy}),
+    case get_castle_type(Piece, Op) of
+        ?CASTLE_WHITE_SHORT -> apply_castle_white_short(State, Op);
+        ?CASTLE_WHITE_LONG  -> apply_castle_white_long(State, Op);
+        ?CASTLE_BLACK_SHORT -> apply_castle_black_short(State, Op);
+        ?CASTLE_BLACK_LONG  -> apply_castle_black_long(State, Op);
+        ?CASTLE_NOT         -> apply_simple(State, Op)
+    end.
+
+-spec get_castle_type(Piece, Op) -> Result when
+    Piece :: chess_piece(),
+    Op :: operator(),
+    Result :: castle_t().
+get_castle_type(?WK, {{5,1},{7,1}}) -> ?CASTLE_WHITE_SHORT;
+get_castle_type(?WK, {{5,1},{3,1}}) -> ?CASTLE_WHITE_LONG;
+get_castle_type(?BK, {{5,8},{7,8}}) -> ?CASTLE_BLACK_SHORT;
+get_castle_type(?BK, {{5,8},{3,8}}) -> ?CASTLE_BLACK_LONG;
+get_castle_type(_Piece, _Op) ->        ?CASTLE_NOT.
+
+apply_simple(State, {{Fx, Fy}, {Tx, Ty}} = Op) ->
+    #board_state{to_move = ToMove, board = Board} = State,
+    Piece = get_piece(State#board_state.board, {Fx, Fy}),
+    Nb1 = set_field(Board, Fx, Fy, ?OO),
+    Nb2 = set_field(Nb1, Tx, Ty, Piece),
+    State#board_state{
+      to_move = ?NEG_COL(ToMove),
+      last_move = Op,
+      board = Nb2
+    }.
+
+-spec apply_castle_white_long(State :: #board_state{}, Op :: operator()) ->
+    #board_state{}.
+apply_castle_white_long(State, Op) ->
+    #board_state{to_move = ToMove, board = Board} = State,
+    Nb1 = set_field(Board, 5, 1, ?OO),
+    Nb2 = set_field(Nb1, 4, 1, ?WR),
+    Nb3 = set_field(Nb2, 3, 1, ?WK),
+    Nb4 = set_field(Nb3, 1, 1, ?OO),
+    State#board_state{
+      wk_castled = true,
+      to_move = ?NEG_COL(ToMove),
+      last_move = Op,
+      board = Nb4
+    }.
+
+-spec apply_castle_white_short(State :: #board_state{}, Op :: operator()) ->
+    #board_state{}.
+apply_castle_white_short(State, Op) ->
+    #board_state{to_move = ToMove, board = Board} = State,
+    Nb1 = set_field(Board, 5, 1, ?OO),
+    Nb2 = set_field(Nb1, 6, 1, ?WR),
+    Nb3 = set_field(Nb2, 7, 1, ?WK),
+    Nb4 = set_field(Nb3, 8, 1, ?OO),
+    State#board_state{
+      wk_castled = true,
+      to_move = ?NEG_COL(ToMove),
+      last_move = Op,
+      board = Nb4
+    }.
+
+-spec apply_castle_black_long(State :: #board_state{}, Op :: operator()) ->
+    #board_state{}.
+apply_castle_black_long(State, Op) ->
+    #board_state{to_move = ToMove, board = Board} = State,
+    Nb1 = set_field(Board, 5, 8, ?OO),
+    Nb2 = set_field(Nb1, 4, 8, ?BR),
+    Nb3 = set_field(Nb2, 3, 8, ?BK),
+    Nb4 = set_field(Nb3, 1, 8, ?OO),
+    State#board_state{
+      bk_castled = true,
+      to_move = ?NEG_COL(ToMove),
+      last_move = Op,
+      board = Nb4
+    }.
+
+-spec apply_castle_black_short(State :: #board_state{}, Op :: operator()) ->
+    #board_state{}.
+apply_castle_black_short(State, Op) ->
+    #board_state{to_move = ToMove, board = Board} = State,
+    Nb1 = set_field(Board, 5, 8, ?OO),
+    Nb2 = set_field(Nb1, 6, 8, ?BR),
+    Nb3 = set_field(Nb2, 7, 8, ?BK),
+    Nb4 = set_field(Nb3, 8, 8, ?OO),
+    State#board_state{
+      bk_castled = true,
+      to_move = ?NEG_COL(ToMove),
+      last_move = Op,
+      board = Nb4
+    }.
+
+-spec set_field(Board, X, Y, Piece) -> Result when
+      Board :: board(),
+      X :: integer(),
+      Y :: integer(),
+      Piece :: chess_piece(),
+      Result :: board().
+set_field(Board, X, Y, Piece) ->
+    Pos = ?POS(X, Y),
+    setelement(Pos, Board, Piece).
+
+-spec find_first_piece(Board, ChessPiece) -> Result when
+      Board :: board(),
+      ChessPiece :: chess_piece(),
+      Result :: position().
 find_first_piece(Board, ChessPiece) ->
     find_first_piece(1, Board, ChessPiece).
 
